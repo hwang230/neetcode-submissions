5269c19c1776536ede8c04b1e24150744b1b3f95
hwang230 <149623068+hwang230@users.noreply.github.com>
2026-08-29 18:11:37 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+
+        // sliding window technique
+        for (int i = 0; i < prices.size()-1 ; i++){
+            for (int j = i + 1; j < prices.size(); j++){
+                if (prices[j] - prices[i] > maxProfit){
+                    maxProfit = prices[j] - prices[i];
+                } 
+            }
+        }
+
+
+
+
+        return maxProfit;
+    }
+};
